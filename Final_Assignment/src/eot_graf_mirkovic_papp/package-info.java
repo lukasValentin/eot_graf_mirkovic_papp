24e333144ae5b91ead47b0f212384147c8364ea9
lukas <lukas@LukasNotebook>
2019-01-27 11:52:03 +0100

    added description

--- Final_Assignment/src/eot_graf_mirkovic_papp/package-info.java
+++ Final_Assignment/src/eot_graf_mirkovic_papp/package-info.java
@@ -2,7 +2,9 @@
  * 
  */
 /**
- * @author lukas
+ * @author Lukas Graf, Stefan Mirkovic, Levente Papp
+ * 
+ * @purpose	This package can be used to visualize the content of Tweets using Google Earth
  *
  */
 package eot_graf_mirkovic_papp;
