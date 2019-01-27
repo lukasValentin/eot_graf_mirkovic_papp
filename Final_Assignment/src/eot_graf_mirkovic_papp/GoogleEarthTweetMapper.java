6ba6a9b2347aa0dce3e185c7ad707b1225e7c7fd
lukas <lukas@LukasNotebook>
2019-01-27 11:45:29 +0100

    added content

--- Final_Assignment/src/eot_graf_mirkovic_papp/GoogleEarthTweetMapper.java
+++ Final_Assignment/src/eot_graf_mirkovic_papp/GoogleEarthTweetMapper.java
@@ -7,7 +7,10 @@ package eot_graf_mirkovic_papp;
 public class GoogleEarthTweetMapper {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//calls the required classes to perform the task
+		
+		//to do
 
 	}
 
