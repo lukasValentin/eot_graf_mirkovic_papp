0c525debabfd37c86b423a184d8129674c96e4f7
lukas <lukas@LukasNotebook>
2019-02-01 08:38:35 +0100

    uploaded additional jars for WMS support

--- Final_Assignment/src/eot_graf_mirkovic_papp/WMSConnector.java
+++ Final_Assignment/src/eot_graf_mirkovic_papp/WMSConnector.java
@@ -1,5 +1,13 @@
 package eot_graf_mirkovic_papp;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.geotools.data.wms.request.GetMapRequest;
+import org.geotools.data.wms.WebMapServer;
+import org.geotools.ows.ServiceException;
+
 /*
  * this public class connects to a WMS service 
  * and retrieves a basemap within a predifined bounding
@@ -7,5 +15,36 @@ package eot_graf_mirkovic_papp;
  */
 
 public class WMSConnector {
+	
+	
+	public int connect2WMS(String URLString_, double[] bbox_) {
+		
+		//setup the server connection using URL
+		URL url = null;
+		
+		//surround by try-catch in case URL to WMS-Server is not working
+		try {
+			 url = new URL(URLString_);
+		} catch (MalformedURLException e) {
+			  System.out.println("The URL you provided is invalid!");
+			  e.printStackTrace();
+		}
+		
+		//now a WMS object is created using the provided URL
+		WebMapServer wms = null;
+		try {
+			wms = new WebMapServer(url);
+		} catch (ServiceException e) {
+			System.out.println("The WMS-Service connection returned an error!");
+			e.printStackTrace();
+		} catch (IOException e1) {
+			System.out.println("There was an IOError with the WMS-Service!");
+			e1.printStackTrace();
+		}
+				
+		// a GetMap-Request object is created using the established wms connection
+		GetMapRequest mapRequest = wms.createGetMapRequest();
+	}
+	
 
 }
