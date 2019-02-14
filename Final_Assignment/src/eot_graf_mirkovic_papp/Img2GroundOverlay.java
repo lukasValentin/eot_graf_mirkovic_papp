ab762893c7701fedc7d5ecf3bc46a80878dd0bcf
Stefan <Stefan@192.168.1.2>
2019-02-14 20:51:14 +0100

    Sample Code (by Stefan) for Groundoverlay

--- Final_Assignment/src/eot_graf_mirkovic_papp/Img2GroundOverlay.java
+++ Final_Assignment/src/eot_graf_mirkovic_papp/Img2GroundOverlay.java
@@ -8,4 +8,24 @@ package eot_graf_mirkovic_papp;
 
 public class Img2GroundOverlay {
 
+	/*
+	 * Sample Code (Salzburg is used)
+	 * 
+	 * <?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+<GroundOverlay>
+	<name>Stadt Salzburg</name>
+	<description>ich bin die Stadtteile Salzburgs.</description>
+	<Icon>
+		<href>https://upload.wikimedia.org/wikipedia/commons/0/00/Salzburger_Stadtteile.jpg</href>
+		<viewBoundScale>0.75</viewBoundScale>
+	</Icon>
+	<LatLonBox>
+		<north>47.85589438482411</north>
+		<south>47.75037379569893</south>
+		<east>13.12769948076074</east>
+		<west>12.98322792797626</west>
+	</LatLonBox>
+</GroundOverlay>
+</kml>*/
 }
