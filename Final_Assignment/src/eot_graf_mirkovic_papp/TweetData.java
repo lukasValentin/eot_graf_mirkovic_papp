d5031ab8fae0058b34776b036b94b5ef03215766
Stefan <Stefan@192.168.1.2>
2019-02-15 12:32:18 +0100

    forgot to add TweetData.java-file

---
+++ Final_Assignment/src/eot_graf_mirkovic_papp/TweetData.java
@@ -0,0 +1,10 @@
+package eot_graf_mirkovic_papp;
+
+// this class contains data of one tweet
+// the purpose of this class is to merge coordinates and the columns "tweet" & "created_at" = kind of container for ONE tweet
+public class TweetData {
+	public String tweet;
+	public String created_at;
+	public double lon;
+	public double lat;
+}
